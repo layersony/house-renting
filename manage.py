ea5f7af1d474d464632c368e3188940438ec14b1
layersony <sammaingi5@gmail.com>
2021-06-21 17:13:33 +0300

    database working

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Roles, UserRoles, House, Review
 
 
 app = create_app('development')
 
-migrate = Migrate(app)
+migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('server', Server)
 manager.add_command('db', MigrateCommand)
@@ -13,7 +14,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-  return dict(app = app)
+  return dict(app = app, db=db, user=User, Roles=Roles, UserRoles=UserRoles, House=House, Review=Review)
 
 if __name__ == '__main__':
   manager.run()
