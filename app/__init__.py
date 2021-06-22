7953a087c252d553e8705ed41d7c62a421259172
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-22 10:30:59 +0300

    reg & login routes for admin and rendering them

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,43 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import config_options
-from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+
 
 db = SQLAlchemy()
+mail = Mail()
+bootstrap = Bootstrap()
+photos = UploadSet('photos', IMAGES)
+
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-bootstrap = Bootstrap()
 
 def create_app(config_name):
   app = Flask(__name__)
 
   app.config.from_object(config_options[config_name])
 
-  db.init_app(app)
   login_manager.init_app(app)
+  db.init_app(app)
   bootstrap.init_app(app)
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
   from .auth import auth as auth_blueprint
+  
   app.register_blueprint(auth_blueprint, url_prefix= '/authenticate')
+
+  from .request import configure_request
+  configure_request(app)
+  
+  configure_uploads(app,photos)
 
   return app
   
---
+++ app/auth/views.py
@@ -0,0 +1,33 @@
+from flask import render_template,redirect,url_for, flash,request
+from app.auth import auth
+from ..models import User
+from .forms import LoginFormAdmin, RegistrationFormAdmin,RegistrationFormAgent
+from .. import db
+from flask_login import login_user
+
+# Admin reg
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationFormAdmin()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Admin Account"
+    return render_template('auth/admin/register.html',registration_form = form)
+
+# Admin login 
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginFormAdmin()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Admin login"
+    return render_template('auth/admin/login.html',login_form = login_form,title=title)
