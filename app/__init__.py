81d548910bb4b087fa0d78975e1873186dcb0b6f
layersony <sammaingi5@gmail.com>
2021-06-23 10:13:24 +0300

    styling for review pages

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -15,6 +17,7 @@ def create_app(config_name):
 
   db.init_app(app)
   login_manager.init_app(app)
+  bootstrap.init_app(app)
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
--- app/main/views.py
+++ app/main/views.py
@@ -1,29 +1,42 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request
 from . import main
 from ..models import User, House, Roles, UserRoles, Review
 from .forms import ReviewForm
 
 @main.route('/')
 def index():
-  heading = 'Working.. Good to go'
-  return render_template('index.html', heading=heading)
+  searchhouse = request.args.get('search')
+  if searchhouse:
+    redirect(url_for('main.searchhouse', locationname = searchhouse))
+  return render_template('index.html')
 
 @main.route('/houselist', methods=['GET', 'POST'])
 def houselist():
   p = 'trial'
   return render_template('houselisting.html', p=p)
 
-@main.route('/review/<id>', methods=['GET', 'POST'])
-def review(id):
-  house = House.query.filter(id=id).first()
+@main.route('/houselist/house', methods=['GET', 'POST'])
+def house():
+  # gethouse = House.query.filter_by(id=id).first()
+  return render_template('viewhouse.html')
+
+@main.route('/review', methods=['GET', 'POST'])
+def review():
+  # house = House.query.filter(id=id).first()
   reviehouse = ReviewForm()
 
-  if review.validate_on_submit():
-    reviews = ReviewForm.reviewinput.data
-    new_review = Review(reviews = reviews)
+  if reviehouse.validate_on_submit():
+    reviews = reviehouse.reviewinput.data
+    rating = reviehouse.ratinginput.data
+    new_review = Review(reviews = reviews, rating=rating)
     new_review.save_review()
     return redirect(url_for('main.houselist'))
   
   pastreview = Review.query.all()
-  return render_template('review.html', reviehouse = reviehouse, house=house, pastreview=pastreview)
-  
+  return render_template('review.html', reviehouse = reviehouse, pastreview=pastreview)
+
+@main.route('/search/house/<locationname>')
+def searchhouse(locationname):
+  # house = House.query.filter_by(location = locationname).all()
+  house = 'house will display' + locationname
+  return render_template('searchhouse.html', house = house)
