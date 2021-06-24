6b4f45bd18545f888f00d1af44a1f7db520ccde3
AbdiazizH <a.aziz69890@gmail.com>
2021-06-24 08:30:09 +0300

    add changes to new_house.html file

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,44 @@
 from flask import render_template
 from . import main
+from flask import render_template,abort,request,redirect,url_for,flash
+from flask_login import login_required,current_user
+from ..models import User,House
+from .. import db
 
-@main.route('/')
+
+
+
+@main.route('/home')
 def index():
-  heading = 'Working.. Good to go'
-  return render_template('index.html', heading=heading)
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    return render_template('index.html' )
+
+@main.route('/admin/index')
+def index_admin():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # house=Houses.get_all_houses()
+    return render_template('admin/index.html' )
+
+
+@main.route('/admin/house/new',methods= ['GET','POST'])
+# @login_required
+def new_house():
+    
+    if request.method=='POST':
+        house_location=request.form['location']
+        house_city=request.form['city']
+        house_price=request.form['price']
+        house_desc=request.form['desc']
+        filename = photos.save(request.files['images'])
+        house_image=f'images/{filename}'
+        
+        new_house=house(house_location=house_location,house_city=house_city,house_price=house_price,house_image=house_image,user=current_user)
+        new_house.save_house()
+
+    return render_template('admin/new_house.html') 
+
--- migrations/versions/def753b21190_initial_migration.py
+++ migrations/versions/def753b21190_initial_migration.py
@@ -1,8 +1,8 @@
-"""initial migration
+"""Initial Migration
 
-Revision ID: 089df9a9a3bb
+Revision ID: def753b21190
 Revises: 
-Create Date: 2021-06-21 17:12:39.153730
+Create Date: 2021-06-23 18:13:53.607680
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '089df9a9a3bb'
+revision = 'def753b21190'
 down_revision = None
 branch_labels = None
 depends_on = None
